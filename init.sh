50bc83a28401e567317dee1e3e462052808bb3e1
duza <duza@tut.by>
2016-07-10 16:37:13 +0300

    A little change for local environment

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -fs /home/goldman/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx start
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
