ecc67c3b5cdec3829cd3cf04c433b08d018069e7
duza <duza@tut.by>
2016-07-10 18:21:02 +0300

    Fixed for my local environment and added line with sed in init.h

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -fs /home/goldman/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx start
+#sed 's/goldman\/stepic_web_project/box\/web/g'-i /home/box/web/etc/nginx.conf
+sudo ln -fs /home/goldman/stepic_web_project/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
-#mkdir public public/js public/img public/css uploads etc
