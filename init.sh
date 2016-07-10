3ee1f27da82aa4772cb2e13d91f04205e38da851
duza <duza@tut.by>
2016-07-10 18:43:12 +0300

    Fixed init.sh and added file which replace my_local_env on stepic_env

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-#sed 's/goldman\/stepic_web_project/box\/web/g'-i /home/box/web/etc/nginx.conf
 sudo ln -fs /home/goldman/stepic_web_project/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
---
+++ replace.sh
@@ -0,0 +1,2 @@
+sed 's/goldman\/stepic_web_project/box\/web/g' -i /home/box/web/etc/nginx.conf
+sed 's/goldman\/stepic_web_project/box\/web/g' -i /home/box/web/init.sh
