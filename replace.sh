39f35ae02233d9fe5489778a0168cc9cacf1a51f
duza <duza@tut.by>
2016-07-14 16:01:26 +0300

    update for gunicorn's config file

--- replace.sh
+++ replace.sh
@@ -1,2 +1,3 @@
 sed 's/goldman\/stepic_web_project/box\/web/g' -i /home/box/web/etc/nginx.conf
+sed 's/goldman\/stepic_web_project/box\/web/g' -i /home/box/web/etc/gunicorn.conf
 sed 's/goldman\/stepic_web_project/box\/web/g' -i /home/box/web/init.sh
