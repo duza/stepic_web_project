3b928dfe5176819184bfc4c2e5318d37474ca508
duza <duza@tut.by>
2016-07-18 11:04:54 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 def wsgi_app(environ, start_response):
     if environ['REQUEST_METHOD'] == 'GET':
-        body = environ['QUERY_STRING'].split('&')
+        body = environ['QUERY_STRING'].replace('&','\n')
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
         ]
     start_response(status, headers)
-    return iter(body)
+    return [body]
