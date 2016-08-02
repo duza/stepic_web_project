435cf94bbbf5a73b8370f9e032d03ff5065f80d9
duza <duza@tut.by>
2016-08-02 23:47:29 +0300

    update init.sh
    changed on relative paths

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -fs /home/goldman/stepic_web_project/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -fs etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -fs /home/goldman/stepic_web_project/etc/gunicorn-wsgi.conf   /etc/gunicorn.d/test-wsgi
-sudo ln -fs /home/goldman/stepic_web_project/etc/gunicorn-django.conf   /etc/gunicorn.d/test-django
+sudo ln -fs etc/gunicorn-wsgi.conf   /etc/gunicorn.d/test-wsgi
+sudo ln -fs etc/gunicorn-django.conf   /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
