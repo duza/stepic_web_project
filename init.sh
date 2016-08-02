29cc8d224933d476ef03085aad837b51902b6bfb
duza <duza@tut.by>
2016-08-02 23:28:58 +0300

    update init.sh
    Added gunicorn django conf

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -fs /home/goldman/stepic_web_project/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -fs /home/goldman/stepic_web_project/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -fs /home/goldman/stepic_web_project/etc/gunicorn-wsgi.conf   /etc/gunicorn.d/test-wsgi
+sudo ln -fs /home/goldman/stepic_web_project/etc/gunicorn-django.conf   /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
