1bef648f1b2489556e411a563d0d49b3c1d7970c
duza <duza@tut.by>
2016-09-29 14:54:43 +0300

    Added models Question, Answer, User

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=300)
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.OneToOneField(User, null=False, on_delete=models.DO_NOTHING)
+    likes = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.OneToOneField(Question, null=False, on_delete=models.DO_NOTHING)
+    author = models.OneToOneField(User, null=False, on_delete=models.DO_NOTHING)
+    
+class User(User)
