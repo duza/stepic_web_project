253d3107a8e597c23c217473f1d052308b5eb9b5
duza <duza@tut.by>
2016-10-11 00:01:45 +0300

    Create local settings

---
+++ ask/ask/local_settings.py
@@ -0,0 +1,14 @@
+from settings import BASE_DIR
+
+STATIC_URL = BASE_DIR + '/static/'
+TEMPLATES_DIR = BASE_DIR + '/templates/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysqldb',
+        'NAME': 'django_db',
+        'USER': 'jeffrey',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
