d7ef59367c7a9cdb673ca98b02a82eeddeca7e9b
duza <duza@tut.by>
2016-10-10 23:29:16 +0300

    update replace.sh

--- replace.sh
+++ replace.sh
@@ -2,3 +2,4 @@ sed 's/goldman\/stepic_web_project/box\/web/g' -i etc/nginx.conf
 sed 's/goldman\/stepic_web_project/box\/web/g' -i etc/gunicorn-wsgi.conf
 sed 's/goldman\/stepic_web_project/box\/web/g' -i etc/gunicorn-django.conf
 sed 's/goldman\/stepic_web_project/box\/web/g' -i ./init.sh
+sed 's/-p //g' -i ./sqlcommands.sh
